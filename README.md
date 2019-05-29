977863087798af31b57727b9f7f63cf87759e5b6
kf-sergiuszwoznicki <swoznicki@objectivity.co.uk>
2019-05-29 13:35:54 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Mendix.InputTypeSetter
-Mendix Text Box widget always sets input type to "text" (or "password" for Hashed strings). This widget fixes this to "number" for numeric fields. You can also set any attribute values using CSS classes like "set-type-email" or "set-min-0".
+# Input Type Setter
+
+Mendix Text Box widget always sets `input.type` to `"text"` (or `password` for Hashed strings). This widget fixes this issue by setting it to `"number"` for **numeric fields** (of type Integer, Long and Decimal).
+
+You can also set **any** other `type` (or other attributes) value of `input` tags using CSS classes like `"set-type-email"` or `"set-min-0"`.
+
+Additionally, it fixes another issue with missing `label.for="{related-input.id}"` in horizontal form orientation.
+
+## Typical usage scenario
+
+It's useful especially for [mobile applications where keyboard type depends](http://mobileinputtypes.com/) on `input.type`, e.g.:
+- **numeric keyboard** for `input.type="number"`
+- **phone keyboard** for `input.type="tel"`
+- **email keyboard** (with `@` and `.com` keys) for `input.type="email"`
+- **url keyboard** (with `/` and `.com` keys) for `input.type="url"`
+
+
+## Usage
+
+Place the widget on your form page and provide the first settings:
+- **Widgets selector**: Selector of widget elements wrapping input tags to change.
+  - Default setting affects all Text Box widgets: `"div.form-group[class*=mx-name-textBox]"`.
+  - You can e.g. extend it for other widgets by removing "class begins with" extension: `"div.form-group"`
+
+This setting alone is enough for changing `input.type` to `"number"` for **numeric fields** (of type Integer, Long and Decimal).
+
+If you need to use other `input.type` (e.g. `"email"` or `"phone"`) or to set other attributes (e.g. `min`, `max`, `step` that make sense for `"number"` and `"range"` inputs), **add special CSS classes to related input widgets**. The class name format is:
+```
+[<prefix>]<attributeName><separator><value>
+```
+The `<prefix>` is optional, so it may be omitted:
+```
+<attributeName><separator><value>
+```
+And configure two further settings:
+- **CSS classes prefix** for `<prefix>`,
+- **Parts separator** for `<separator>`.
+
+By default, `<prefix>` is `"set-"` and `<separator>` is `"-"`, so for example:
+- using class `"set-type-email"` will set attribute `type` to `"email"`
+- using class `"set-min-0"` will set attribute `min` to `"0"`.
---
+++ src/InputTypeSetter/widget/InputTypeSetter.js
@@ -0,0 +1,87 @@
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dojo/_base/lang",
+    "dojo/dom-attr",
+
+], function (declare, _WidgetBase, lang, dojoAttr) {
+    "use strict";
+
+    return declare("InputTypeSetter.widget.InputTypeSetter", [ _WidgetBase ], {
+
+        // modeler params
+        widgetsSelector: '',
+        prefix: 'set-',
+        separator: '-',
+
+        postCreate: function () {
+            //logger.debug(this.id + ".postCreate");
+            this.domNode.style.display = "none";
+            this.widgetsSelector = this.widgetsSelector || 'div.form-group[class*=mx-name-textBox]';
+        },
+
+        update: function (obj, callback) {
+            //logger.debug(this.id + ".update");
+            this._updateRendering(callback);
+        },
+
+        _updateRendering: function (callback) {
+            //logger.debug(this.id + "._updateRendering");
+            this._setAttributes();
+            this._executeCallback(callback, "_updateRendering");
+        },
+
+        // Shorthand for executing a callback, adds logging to your inspector
+        _executeCallback: function (cb, from) {
+            //logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        },
+
+        // set single input attribute
+        _setAttrib: function (targetElem, attrib, value) {
+            logger.debug(this.id + ': setting ' + attrib + '="' + value + '" for ' + targetElem.tagName + '#' + targetElem.id);
+            dojoAttr.set(targetElem, attrib, value);
+        },
+
+        // main job function
+        _setAttributes: function () {
+            //logger.debug(this.id + "._setAttributes");
+            var _this = this;
+            var parents = document.querySelectorAll(this.widgetsSelector);
+            parents.forEach(function (parent) {
+
+                // set by widgetid staring with mxui_widget_NumberInput_
+                var numberInput = parent.querySelector('input[type=text][widgetid^="mxui_widget_NumberInput_"]');
+                if (numberInput) {
+                    _this._setAttrib(numberInput, 'type', 'number');
+                }
+
+                // set by CSS class
+                parent.className.split(' ')
+                    .filter(function (cssClass) {return cssClass.startsWith(_this.prefix)})
+                    .forEach(function (cssClass) {
+                        var parts = cssClass.replace(_this.prefix, '').split(_this.separator);
+                        if (parts.length == 2) {
+                            var attrib = parts[0];
+                            var value = parts[1];
+                            var targetInputs = parent.querySelectorAll('input');
+                            targetInputs.forEach(function (targetInput) {
+                                _this._setAttrib(targetInput, attrib, value);
+                            });
+                        }
+                    });
+
+                // fix missing label.for
+                var labelElem = parent.querySelector('label.control-label');
+                var controlElem = parent.querySelector('.form-control');
+                if (labelElem && controlElem && !dojoAttr.get(labelElem, 'for')) {
+                    _this._setAttrib(labelElem, 'for', dojoAttr.get(controlElem, 'id'));
+                }
+            });
+        }
+    });
+});
+
+require(["InputTypeSetter/widget/InputTypeSetter"]);
